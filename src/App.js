36649aba9b3221913bf02e9f389edc0bc0212bcb
disel-brest <disel-brest@mail.ru>
2020-07-07 16:37:07 +0300

    master

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="wrapper">
+                Header
+                {/* <Header /> */}
+                <div className="contain">
+                    
+                </div>
+
+                
+            </div>
+        );
+    }
+}
+
+export default App;
